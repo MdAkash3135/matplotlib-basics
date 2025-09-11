68cb44c749fe73204a5c4b7f2f84d42877e9e46b
MdAkash3135 <eaqubmia@gmail.com>
2025-09-11 16:31:33 +0600

    FOntdict dict about graph

--- matplot_basics.py
+++ matplot_basics.py
@@ -6,3 +6,12 @@ x = [1,2,3]
 y = [4,5,6]
 plt.plot([1,2,3], [4,5,6])
 plt.show()
+
+
+x = [1,2,3]
+y = [4,5,6]
+plt.plot(x,y)
+plt.title("Our first Graph", fontdict={'fontname': 'Comic Sans MS' , 'fontsize': 20})
+plt.xlabel("X Axis ! ")
+plt.ylabel("Y axis")
+plt.show()
