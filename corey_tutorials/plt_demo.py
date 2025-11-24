dcdca060f283cc90083c3327b1ecdc3075d38810
MdAkash3135 <mdeaqubmia@gmail.com>
2025-11-24 17:14:40 +0600

    plot(core/plot_demo.py) : IMplement basic plot and style it
    Use show, plot, legen , tight_layout , xkcd, xlabel , y label style.use etc
    
    *24NOV25

--- corey_tutorials/plt_demo.py
+++ corey_tutorials/plt_demo.py
@@ -1,13 +1,18 @@
 from matplotlib import pyplot as plt
 
+print(plt.style.available)
+
+plt.xkcd()  # use xkcd style
+
+plt.style.use('Solarize_Light2')
 dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 
 dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
 
-py_dev_y = [50000, 51000, 52000, 53000, 53437, 76373, 82316, 84928, 87317, 88748, 83752]
+py_dev_y = [50000, 51000, 52000, 53000, 54437, 55373, 83160, 84928, 87317, 88748, 83752]
 
-plt.plot(dev_x, dev_y, '--' , color='#444444' , label = "All Developers")
-plt.plot(dev_x, py_dev_y,'b', color = "#9a675a" ,label = "Python Developers")
+plt.plot(dev_x, dev_y, '-.b' , linewidth=5, label = "All Developers")
+plt.plot(dev_x, py_dev_y,'r' ,label = "Python Developers")
 
 
 
@@ -16,6 +21,9 @@ plt.xlabel("Ages")
 plt.ylabel("Median Salary (USD)")
 
 
-plt.legend(["All Developers", "Python Developers"])
+plt.legend()
 
+plt.grid(True)
+plt.tight_layout()
+plt.savefig('statics/plot.png')
 plt.show()
