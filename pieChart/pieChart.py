49a9cbc3eb9e31f12c7cb32ce683ca31f3492cab
MdAkash3135 <mdeaqubmia@gmail.com>
2025-11-26 12:00:58 +0600

    basicPichart: IMplement basic chart with matplotlib 26 Nov 2025

--- pieChart/pieChart.py
+++ pieChart/pieChart.py
@@ -3,7 +3,23 @@ from matplotlib import pyplot as plt
 plt.style.use('fivethirtyeight')
 
 
+slices = [60, 40]
+labels = ['Sixty', 'Forty']
+colors = ['#ff9999','#66b3ff']
 
-plt.title("Pie Chart Example")
+# plt.pie(slices, labels=labels, colors=colors, wedgeprops={'edgecolor': 'black'}, autopct='%1.1f%%')
+# plt.title("Pie Chart Example")
+# plt.tight_layout()
+# plt.show()
+
+
+# Language Popularity
+slices = [59219, 55466, 47544, 36443, 35917]
+labels = ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java' ]
+
+explode = [0, 0 , 0, 0.5, 0]
+
+plt.pie(slices, labels=labels, wedgeprops={'edgecolor': 'black'}, autopct='%1.1f%%', startangle=90, explode=explode, shadow=True, colors=colors)
+plt.title("Most Popular Languages in 2024")
 plt.tight_layout()
 plt.show()
